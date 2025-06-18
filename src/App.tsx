0bb782f42a8e790636fb0d71c3ff27dec79dbd52
arturarushanyan <arthur.arushanyan92@gmail.com>
2025-06-18 10:09:00 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import styles from './App.module.css'
+
+const App: FC = () => {
+  return (
+    <div className={styles.app}>
+      <h1 className={styles.title}>Hello World</h1>
+    </div>
+  )
+}
+
+export default App 
